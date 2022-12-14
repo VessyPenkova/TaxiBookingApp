eadf59282e58fcd310f9b4e7b752be9943a0d24d
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2022-12-14 19:23:28 +0200

    Office controller added

---
+++ TaxiBookingApp/Areas/Admin/Controllers/OfficeController.cs
@@ -0,0 +1,23 @@
+﻿using HouseRentingSystem.Core.Services.Admin;
+using Microsoft.AspNetCore.Mvc;
+using TaxiBookingApp.Controllers;
+using TaxiBookingApp.Core.Contracts.Admin;
+
+namespace TaxiBookingApp.Areas.Admin.Controllers
+{
+    public class OfficeController : BaseController
+    {
+        private readonly IOfficeService officeService;
+        public OfficeController(IOfficeService _officeService)
+        {
+            officeService = _officeService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var model = await officeService.All();
+
+            return View(model);
+        }
+    }
+}
