9132505623dd0ec58a0fa9c68cfa3b4c9e8befdc
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2022-12-15 00:19:31 +0200

    Extentions Ammended

--- TaxiBookingApp/Extensions/TaxiBookingAppServiceCollectionExtension.cs
+++ TaxiBookingApp/Extensions/TaxiBookingAppServiceCollectionExtension.cs
@@ -3,7 +3,7 @@ using TaxiBookingApp.Core.Contracts;
 using TaxiBookingApp.Core.Contracts.Admin;
 using TaxiBookingApp.Core.Exceptions;
 using TaxiBookingApp.Core.Services;
-
+using TaxiBookingApp.Core.Services.Admin;
 using TaxiBookingApp.Infrastructure.Data.Common;
 
 namespace TaxiBookingApp.Extensions
@@ -17,6 +17,7 @@ namespace TaxiBookingApp.Extensions
             services.AddScoped<IDriverCarService, DriverCarService>();
             services.AddScoped<IGuard, Guard>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IOfficeService, OfficeService>();
 
             return services;
         }
